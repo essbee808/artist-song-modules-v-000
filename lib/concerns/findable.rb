c0e883c44467f04e4fe7496a5683c74fcc0a4a85
Sheena Bonilla <sheena.myb@gmail.com>
2018-09-20 05:04:09 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,7 +3,7 @@ module Findable
   module ClassMethods
     
     def find_by_name(name)
-      retur self.all.detect{|a| self.name == name}
+      return self.all.detect{|a| self.name == name}
     end
   end
   
