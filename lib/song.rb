959faa8d10d947599fa3c7c0ff0971b28b8a5329
Sheena Bonilla <sheena.myb@gmail.com>
2018-09-20 05:20:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   @@array = []
 
   def initialize
-    @@songs << self
+    @@array << self
   end
 
   # def self.find_by_name(name)
@@ -19,7 +19,7 @@ class Song
   # end
 
   def self.all
-    @@songs
+    @@array
   end
 
   def artist=(artist)
