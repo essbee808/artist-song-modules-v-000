be84e7e2443e57f72bef8b6bbb433c697fc9e752
Sheena Bonilla <sheena.myb@gmail.com>
2018-09-20 05:34:00 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -13,6 +13,7 @@ module Memorable
   module InstanceMethods
     def initialize
       self.class.all << self
+      #=> the first 'self' refers to the instance of the class on which I am operating on
     end 
   end
 end
